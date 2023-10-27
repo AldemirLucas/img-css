9d43937290fc2166d6381ecfda40999c492df116
AldemirLucas <aldemirlucas23@gmail.com>
2023-10-27 01:45:28 -0300

    wip art

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <span className="wave"/>
+
+      <div className="content">
+        <p className="star-TL">+</p>
+        <p className="star-TR">+</p>
+
+        <div className="mask-base">
+          <div className="base">
+            
+            <div className="horizon-line"/>
+
+          </div>
+        </div>
+
+        <div className="planets-BL">
+          <div className="planet-small"/>
+          <div className="planet-medium"/>
+          <div className="planet-big"/>
+        </div>
+
+        <div className="planets-BR">
+          <div className="planet-big"/>
+          <div className="planet-medium"/>
+          <div className="planet-small"/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
